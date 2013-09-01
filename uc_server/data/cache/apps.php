d8cf7fc5c77abee458b110b9b18abbfcfb6d535a
gerpayt <gerpayt@qq.com>
2013-09-01 09:59:13 +0800

    first

---
+++ uc_server/data/cache/apps.php
@@ -0,0 +1,29 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'OTHER',
+    'name' => 'demo',
+    'url' => 'http://127.0.0.1/uc2dmo/',
+    'authkey' => '36e7gNTd0jFdvqgzZn2yyrmyuGTGpQApEJuzj5s',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => 
+    array (
+      'apppath' => '',
+    ),
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+);
+
+?>
